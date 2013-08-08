657a990fccf92368df81dad7d25b0869804ea9d9
Shaun Luttin <shaunluttin@gmail.com>
2013-08-08 16:41:12 -0700

    Yet another rename.

--- googlemaps.js
+++ googlemaps.js
@@ -14,11 +14,11 @@ function initialize() {
 
     var rooturl = 'http://castaway.azurewebsites.net/mapdata/';
 
-    var kml1 = new google.maps.KmlLayer(rooturl + 'activity_280222495-processed-v25.kml');
+    var kml1 = new google.maps.KmlLayer(rooturl + 'kml1.kml');
     kml1.setMap(map);
 
-    // var kml2 = new google.maps.KmlLayer(rootUrl + 'activity_297019104-processed-v3.kml');
-    // kml2.setMap(map);
+    var kml2 = new google.maps.KmlLayer(rooturl + 'kml2.kml');
+    kml2.setMap(map);
 
     var listener = google.maps.event.addListener(map, "idle", function () {
         map.setZoom(12);
