9c0f690993bbf9dd9d01e84e6b677e04a7abe738
Shaun Luttin <shaunluttin@gmail.com>
2013-08-08 16:42:24 -0700

    Another odd rename.

--- googlemaps.js
+++ googlemaps.js
@@ -17,8 +17,8 @@ function initialize() {
     var kml1 = new google.maps.KmlLayer(rooturl + 'kml1.kml');
     kml1.setMap(map);
 
-    var kml2 = new google.maps.KmlLayer(rooturl + 'kml2.kml');
-    kml2.setMap(map);
+    //var kml2 = new google.maps.KmlLayer(rooturl + 'kml2.kml');
+    //kml2.setMap(map);
 
     var listener = google.maps.event.addListener(map, "idle", function () {
         map.setZoom(12);
