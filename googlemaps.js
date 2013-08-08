654a7ea69e0205ef6a3c0d81d0b51035500be90a
Shaun Luttin <shaunluttin@gmail.com>
2013-08-08 16:34:17 -0700

    Rename.

--- googlemaps.js
+++ googlemaps.js
@@ -13,7 +13,7 @@ function initialize() {
     var map = new google.maps.Map(document.getElementById("map-canvas"));
 
     var rooturl = 'http://castaway.azurewebsites.net/mapdata/';
-    var activity1 = new google.maps.KmlLayer(rooturl + 'activity_280222495-processed-v18.kml');
+    var activity1 = new google.maps.KmlLayer(rooturl + 'activity_280222495-processed-v20.kml');
     // var activity2 = new google.maps.KmlLayer(rootUrl + 'activity_297019104-processed-v3.kml');
 
     activity1.setMap(map);
