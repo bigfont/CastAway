fe3c23b770eb1bde7dbbe8977b9dbe3508142275
Shaun Luttin <shaunluttin@gmail.com>
2013-08-09 10:33:17 -0700

    Use a combined KML document, and enable zooming.

--- googlemaps.js
+++ googlemaps.js
@@ -1,26 +1,19 @@
 ﻿
 
-function addKmlToMap(map, myLatLng, kmlFileName) {
+function addKmlToMap(map, kmlFileName) {
     var kml;
     kml = new google.maps.KmlLayer(kmlFileName);
     kml.setMap(map);
-
-    listener = google.maps.event.addListener(map, "idle", function () {
-        map.setZoom(12);
-        map.setCenter(myLatLng);
-        map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
-        google.maps.event.removeListener(listener);
-    });
 }
 
 function initialize() {
 
     var myLatLng, map, listener;
-    myLatLng = new google.maps.LatLng(48.800000, -123.527862);
     map = new google.maps.Map(document.getElementById("map-canvas"));
+    map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+
     // these are in DropBox for now, because serving from Azure lead to one of them not working at all.
-    addKmlToMap(map, myLatLng, 'https://dl.dropboxusercontent.com/u/17522409/mapdata/activity_297019104-processed-v3.kml');
-    addKmlToMap(map, myLatLng, 'https://dl.dropboxusercontent.com/u/17522409/mapdata/activity_280222495-processed-v25.kml');
+    addKmlToMap(map, 'https://dl.dropboxusercontent.com/u/17522409/mapdata/activity_combined-v2.kml');
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
