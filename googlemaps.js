b8a7066083bce6cf2e861ae0589ccaa4790d015f
Shaun Luttin <shaunluttin@gmail.com>
2013-08-08 17:01:38 -0700

    Serve from drop box, because for now Azure is not serving properly. Could it be a latency issue?

--- googlemaps.js
+++ googlemaps.js
@@ -1,10 +1,16 @@
 ﻿
 
-function addKmlToMap(map, kmlFileName) {
-    var rooturl, kml;
-    rooturl = 'http://castaway.azurewebsites.net/mapdata/';
-    kml = new google.maps.KmlLayer(rooturl + kmlFileName);
+function addKmlToMap(map, myLatLng, kmlFileName) {
+    var kml;
+    kml = new google.maps.KmlLayer(kmlFileName);
     kml.setMap(map);
+
+    listener = google.maps.event.addListener(map, "idle", function () {
+        map.setZoom(12);
+        map.setCenter(myLatLng);
+        map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+        google.maps.event.removeListener(listener);
+    });
 }
 
 function initialize() {
@@ -12,14 +18,9 @@ function initialize() {
     var myLatLng, map, listener;
     myLatLng = new google.maps.LatLng(48.800000, -123.527862);
     map = new google.maps.Map(document.getElementById("map-canvas"));
-    addKmlToMap(map, 'kml100.kml');
-    addKmlToMap(map, 'kml20.kml');
-    listener = google.maps.event.addListener(map, "idle", function () {
-        map.setZoom(12);
-        map.setCenter(myLatLng);
-        map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
-        google.maps.event.removeListener(listener);
-    });
+    // these are in DropBox for now, because serving from Azure lead to one of them not working at all.
+    addKmlToMap(map, myLatLng, 'https://dl.dropboxusercontent.com/u/17522409/mapdata/activity_297019104-processed-v3.kml');
+    addKmlToMap(map, myLatLng, 'https://dl.dropboxusercontent.com/u/17522409/mapdata/activity_280222495-processed-v25.kml');
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
